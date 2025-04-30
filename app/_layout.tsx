7a3787a5b85458fffe5b2b15b4ae7766038f6753
sasirasys <131147291+Sasirasys@users.noreply.github.com>
2025-04-30 19:16:44 +0700

    Remove dark mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout() {
   // }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
