69fd137922c8ee3583f614a44f1bc25ef67471cf
vinitian <140070514+vinitian@users.noreply.github.com>
2025-05-10 01:59:52 +0700

    chore: remove comments, edit spelling

--- lib/fetchUser.tsx
+++ lib/fetchUser.tsx
@@ -8,12 +8,10 @@ export default function fetchUser({ user_id, setUserData }:{user_id:string, setU
         const docRef = doc(db, "users", user_id);
         const docSnap = await getDoc(docRef);   
       
-        // console.log(docSnap)
-        // console.log(docSnap._document.data.value.mapValue.fields);
         setUserData(docSnap.data());
         
       } catch (err) {  
-        console.log("error fetching user data")
+        console.log("Error fetching user data")
         console.error(err);
       }
     }
