dc0136b86707a5a16656a9768c9d45bea877e207
vinitian <140070514+vinitian@users.noreply.github.com>
2025-05-12 19:06:44 +0700

    chore: delete comments in `app/_layout.tsx`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,29 +8,26 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { createContext, useContext, useEffect, useState } from "react";
-
 import "react-native-reanimated";
-
 import { useColorScheme } from "@/hooks/useColorScheme";
-// import auth, {
-//   FirebaseAuthTypes,
-//   onAuthStateChanged,
-// } from "@react-native-firebase/auth";
 import { ThemedView } from "@/components/ThemedView";
-// import { ActivityIndicator } from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-const AppContext = createContext<{ bookId:string, setBookId:Function } | null>(null)
+const AppContext = createContext<{
+  bookId: string;
+  setBookId: Function;
+} | null>(null);
 
 export function useAppContext() {
-  const context = useContext(AppContext)
+  const context = useContext(AppContext);
   // make sure that the components using this context must be wrapped with the context provider
-  if (!context) { // if context is null i.e. not available
-    throw new Error('useAppContext must be used inside AppContext Provider')
+  if (!context) {
+    // if context is null i.e. not available
+    throw new Error("useAppContext must be used inside AppContext Provider");
   }
-  return context
+  return context;
 }
 
 export default function RootLayout() {
@@ -38,25 +35,8 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
-  // const [initializing, setInitializing] = useState(true);
-  // const [user, setUser] = useState<FirebaseAuthTypes.User | null>();
 
-  // const onAuthStateChanged1 = (user: FirebaseAuthTypes.User | null) => {
-  //   console.log("onAuthStateChanged", user);
-  //   setUser(user);
-  //   if (initializing) setInitializing(false);
-  // };
-
-  // useEffect(() => {
-  //   // const subscriber = auth().onAuthStateChanged(onAuthStateChanged1);
-  //   onAuthStateChanged(auth(), (user) => {
-  //     onAuthStateChanged1(user);
-  //     return user;
-  //   });
-  //   // return subscriber;
-  // }, []);
-
-  const [ bookId, setBookId ] = useState("EsaXX8v0ywUEyiD9KoFs") // default selected book is "An Anthology of Australian Albums" 
+  const [bookId, setBookId] = useState("EsaXX8v0ywUEyiD9KoFs"); // default selected book is "An Anthology of Australian Albums"
 
   useEffect(() => {
     if (loaded) {
@@ -68,22 +48,8 @@ export default function RootLayout() {
     return null;
   }
 
-  // if (initializing) {
-  //   return (
-  //     <ThemedView
-  //       style={{
-  //         alignItems: "center",
-  //         justifyContent: "center",
-  //         flex: 1,
-  //       }}
-  //     >
-  //       <ActivityIndicator size="large" />
-  //     </ThemedView>
-  //   );
-  // }
-
   return (
-    <AppContext.Provider value= {{ bookId, setBookId }}>
+    <AppContext.Provider value={{ bookId, setBookId }}>
       <ThemeProvider value={DefaultTheme}>
         <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
